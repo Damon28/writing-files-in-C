848dcf11c502b12783262c44ed4a0e8375d44bf3
DMDay3 <damon.maldonado.1@gmail.com>
2021-07-16 10:53:39 -0700

    Writing Files using fopen(), fclose(), & modes (r, w, & a)

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*https://www.youtube.com/watch?v=KJgsSFOSQv0*/
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("\n===Writing Files in C===\n\n");
+    printf("For fopen(): 1st param is the name of the file; 2nd param is the mode\nfopen(\"<file>\", <mode>)\n\n");
+    printf("MODES:\nr - read\nw - write (overwrites)\na - append");
+    FILE * fpointer = fopen("employees.txt", "w"); /*fpointer is a pointer to the mem addr of the file (employees.txt)*/
+
+    fprintf(fpointer, "Jim, Salesman\nPam, Receptionist\nOscar, Accounting"); /*NOTE: fprintf() takes 2 parameters*/
+
+    fclose(fpointer);
+    printf("\n\nNOTE: always use fclose() once done w/ the file.\n\n");
+
+    printf("Adding Kelly from customer service to 'employees.txt\n\n'");
+
+    fopen("employees.txt", "a");
+
+    fprintf(fpointer, "\nKelly, Customer Service\n\n");
+
+    fclose(fpointer);
+
+
     return 0;
 }
